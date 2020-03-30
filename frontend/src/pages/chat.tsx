21122cfbe596b6172af9c0fd6b625fb4c9078ab6
Jenny Hu <huj@mit.edu>
2020-03-30 15:28:33 -0700

    backend connects to frontend

--- frontend/src/pages/chat.tsx
+++ frontend/src/pages/chat.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'evergreen-ui';
 
 export default function App(){
+    
+    const [chats, setChats] = useState("");
+
+    async function handleClick(){
+        // console.log('handlings lots of clicks');
+        let response = await fetch("/rob");
+        let res_text = await response.text();
+        // console.log(await response.text());
+        setChats(chats + "\n" + res_text);
+    }
+    
     return (
         <div>
             Chatting!
+            <p>These are the chats: {chats}</p>
+            <Button onClick={handleClick} appearance="primary"> Send Request to API </Button>
         </div>
     )
 }
--- frontend/src/pages/welcome.tsx
+++ frontend/src/pages/welcome.tsx
@@ -7,9 +7,9 @@ import {
 
 export default function App(){
     return (
-        <Pane display = 'flex' height = '100vh'>
-            <Pane margin = 'auto'>
-                <Link to ='/chat'>
+        <Pane display='flex' height='100vh'>
+            <Pane margin='auto'>
+                <Link to='/chat'>
                     <Button appearance="primary"> Start Chatting! </Button>
                 </Link>
             </Pane>
