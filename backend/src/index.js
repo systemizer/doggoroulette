9c5a7e761c646e22d887247aa221ba0e407dee47
Rob McQueen <robmcqn@gmail.com>
2020-04-01 14:39:29 -0700

    dont listen to 0.0.0.0

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,6 +36,6 @@ app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/../../frontend/build/index.html'));
 });
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
     console.log(`server started at http:// ${PORT}`);
 });
