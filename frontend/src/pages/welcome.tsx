cd132f71b7295d5fc55e867e19e18d0bd6249df1
Rob McQueen <robmcqn@gmail.com>
2020-04-09 14:07:26 -0700

    updating welcome page to look pretty

--- frontend/src/pages/welcome.tsx
+++ frontend/src/pages/welcome.tsx
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
 import Websocket from "react-websocket";
 import { withRouter, RouteComponentProps } from "react-router-dom";
-import {
-    Button,
-    Pane,
-    Heading,
-    majorScale,
-    Text
-} from "evergreen-ui";
+import { Button, Pane, Heading, majorScale, Text } from "evergreen-ui";
 import { host } from "../config";
 import { Chatroom } from "../models";
 
@@ -24,7 +18,13 @@ function App(props: RouteComponentProps) {
     }
 
     return (
-        <Pane backgroundColor="snow" height="100vh" width="100%" display="flex" flexDirection="row">
+        <Pane
+            backgroundColor="snow"
+            height="100vh"
+            width="100%"
+            display="flex"
+            flexDirection="row"
+        >
             <Pane flex={1} backgroundColor="lemonchiffon" display="flex">
                 <Pane margin="auto" textAlign="center">
                     <Heading
@@ -34,56 +34,36 @@ function App(props: RouteComponentProps) {
                         lineHeight={"74%"}
                         marginBottom={majorScale(5)}
                     >
-                        {"Doggo"}<br />&nbsp;&nbsp;&nbsp;{"Roulette"}
+                        {"Doggo"}
+                        <br />
+                        &nbsp;&nbsp;&nbsp;{"Roulette"}
                     </Heading>
-                    <Heading id="homesubtitle" color={"DarkGoldenRod"} fontSize={"55px"} position="relative" top="-25px" left="80px"
+                    <Heading
+                        id="homesubtitle"
+                        color={"DarkGoldenRod"}
+                        fontSize={"55px"}
+                        position="relative"
+                        top="-25px"
+                        left="80px"
                     >
                         Free Dogs
-		    </Heading>
+          </Heading>
                 </Pane>
             </Pane>
 
-            <Pane flex={1}>
-                <Button
-                    onClick={openWebSocket}
-                    height={40}
-                    isLoading={isQueueing}
-                    appearance="primary"
-                    intent="success"
-                >
-                    {isQueueing ? "Finding a match" : "Start sending doggos!"}
-                </Button>
-
-                <Pane display="flex" paddingTop={majorScale(6)} padding={majorScale(6)}>
-                    <Pane flex={1} padding={majorScale(4)} backgroundColor={"#FFFCE2"}>
-                        <Pane textAlign="center">
-                            <Text fontSize={"70px"}>{"🤝"}</Text>
-                            <Heading marginTop={majorScale(2)} size={700}>
-                                {"Get matched with a stranger"}
-                            </Heading>
-                        </Pane>
-                    </Pane>
-                    <Pane flex={1} margin={majorScale(4)} backgroundColor={"#FFFCE2"}>
-                        <Pane textAlign="center">
-                            <Text fontSize={"70px"}>{"🐕"}</Text>
-                            <Heading marginTop={majorScale(2)} size={700}>
-                                {"Have a convo in Dog Gifs"}
-                            </Heading>
-                        </Pane>
-                    </Pane>
-                    <Pane flex={1} margin={majorScale(4)} backgroundColor={"#FFFCE2"}>
-                        <Pane textAlign="center">
-                            <Text fontSize={"70px"}>{"🦄"}</Text>
-                            <Heading marginTop={majorScale(2)} size={700}>
-                                {"Profit"}
-                            </Heading>
-                            <Text marginTop={majorScale(2)} size={500}>
-                                {
-                                    "Not enough dogs? Initiate a new Doggoroulette and seek the wonders of the universe."
-                                }
-                            </Text>
-                        </Pane>
-                    </Pane>
+            <Pane flex={1} display="flex">
+                <Pane margin="auto" display="flex" flexDirection="column" alignItems="center">
+                    <Text marginBottom={majorScale(4)} lineHeight={"100%"} fontSize="80px">🐶</Text>
+                    <Button
+                        borderRadius="0px"
+                        onClick={openWebSocket}
+                        fontSize={"35px"}
+                        fontFamily="NatureSpirit"
+                        height={80}
+                        isLoading={isQueueing}
+                    >
+                        {isQueueing ? "Finding a match" : "Start sending doggos!"}
+                    </Button>
                 </Pane>
             </Pane>
             <Pane>
@@ -94,7 +74,7 @@ function App(props: RouteComponentProps) {
                     />
                 )}
             </Pane>
-        </Pane>
+        </Pane >
     );
 }
 
